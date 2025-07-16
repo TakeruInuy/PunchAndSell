ff52ed5bb4353f4dc1fddda14d3984eee27f7cf1
TakeruInuy <takeruinuy@gmail.com>
2025-07-16 16:09:38 -0300

    [change] updated build settings

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,7 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if(InputManager.inputDirection.magnitude != 0)
